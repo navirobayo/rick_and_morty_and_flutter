33cc7ffbe7b26aeaad565f0c81ab7021634c1919
Ivan <101908819+navirobayo@users.noreply.github.com>
2023-09-29 01:20:17 -0500

    Character Visualizer Final

--- rick_and_morty_and_flutter/lib/presentation/character_detail_screen/character_detail_screen.dart
+++ rick_and_morty_and_flutter/lib/presentation/character_detail_screen/character_detail_screen.dart
@@ -13,17 +13,73 @@ class CharacterDetailScreen extends StatefulWidget {
 }
 
 class _CharacterDetailScreenState extends State<CharacterDetailScreen> {
+  bool _isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Character Detail')),
-      body: Center(
+      appBar: AppBar(title: const Text('Multidimensional Visualizer')),
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Name: ${widget.character.name}'),
-            Text('Status: ${widget.character.status}'),
-            // Add more details as needed
+            SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(widget.character.image),
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text('Name: ${widget.character.name}'),
+                    Text(' '),
+                    Text('Status: ${widget.character.status}'),
+                    Text(' '),
+                    Text('Species: ${widget.character.species}'),
+                  ],
+                ),
+                SizedBox(width: 25),
+              ],
+            ),
+            SizedBox(height: 50),
+            ExpansionPanelList(
+              elevation: 1,
+              expandedHeaderPadding: EdgeInsets.all(10),
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  _isExpanded = !isExpanded;
+                });
+              },
+              children: [
+                ExpansionPanel(
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return ListTile(
+                      title: Text('Episodes with ${widget.character.name}'),
+                    );
+                  },
+                  body: SingleChildScrollView(
+                    // Wrap the content in SingleChildScrollView
+                    child: Column(
+                      children: widget.character.episode
+                          .map((episodeUrl) => ListTile(
+                                title: Text(episodeUrl),
+                              ))
+                          .toList(),
+                    ),
+                  ),
+                  isExpanded: _isExpanded,
+                ),
+              ],
+            ),
           ],
         ),
       ),
--- rick_and_morty_and_flutter/lib/presentation/search_result_detail_screen/search_result_detail_screen.dart
+++ rick_and_morty_and_flutter/lib/presentation/search_result_detail_screen/search_result_detail_screen.dart
@@ -14,17 +14,74 @@ class SearchResultDetailScreen extends StatefulWidget {
 }
 
 class _SearchResultDetailScreenState extends State<SearchResultDetailScreen> {
+  bool _isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Character Detail')),
-      body: Center(
+      appBar: AppBar(title: const Text('Multidimensional Visualizer')),
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Name: ${widget.searchCharacter.name}'),
-            Text('Status: ${widget.searchCharacter.status}'),
-            // Add more details as needed
+            SizedBox(height: 25),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(widget.searchCharacter.image),
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text('Name: ${widget.searchCharacter.name}'),
+                    Text(' '),
+                    Text('Status: ${widget.searchCharacter.status}'),
+                    Text(' '),
+                    Text('Species: ${widget.searchCharacter.species}'),
+                  ],
+                ),
+                SizedBox(width: 25),
+              ],
+            ),
+            SizedBox(height: 50),
+            ExpansionPanelList(
+              elevation: 1,
+              expandedHeaderPadding: EdgeInsets.all(10),
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  _isExpanded = !isExpanded;
+                });
+              },
+              children: [
+                ExpansionPanel(
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return ListTile(
+                      title:
+                          Text('Episodes with ${widget.searchCharacter.name}'),
+                    );
+                  },
+                  body: SingleChildScrollView(
+                    // Wrap the content in SingleChildScrollView
+                    child: Column(
+                      children: widget.searchCharacter.episode
+                          .map((episodeUrl) => ListTile(
+                                title: Text(episodeUrl),
+                              ))
+                          .toList(),
+                    ),
+                  ),
+                  isExpanded: _isExpanded,
+                ),
+              ],
+            ),
           ],
         ),
       ),
